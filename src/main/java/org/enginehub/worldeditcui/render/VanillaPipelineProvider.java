cd335c55b33a3726999c68b22ff264152db63521
elmital <54907162+elmital@users.noreply.github.com>
2023-05-13 20:08:59 +0200

    fix: thicker lines when looking long an axis

--- src/main/java/org/enginehub/worldeditcui/render/VanillaPipelineProvider.java
+++ src/main/java/org/enginehub/worldeditcui/render/VanillaPipelineProvider.java
@@ -10,8 +10,8 @@ public final class VanillaPipelineProvider implements PipelineProvider {
         public static final DefaultTypeFactory INSTANCE = new DefaultTypeFactory();
 
         private static final BufferBuilderRenderSink.RenderType QUADS = new BufferBuilderRenderSink.RenderType(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR, GameRenderer::getPositionColorShader);
-        private static final BufferBuilderRenderSink.RenderType LINES = new BufferBuilderRenderSink.RenderType(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL, GameRenderer::getRendertypeLinesShader);
-        private static final BufferBuilderRenderSink.RenderType LINES_LOOP = new BufferBuilderRenderSink.RenderType(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL, GameRenderer::getRendertypeLinesShader);
+        private static final BufferBuilderRenderSink.RenderType LINES = new BufferBuilderRenderSink.RenderType(VertexFormat.Mode.DEBUG_LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL, GameRenderer::getRendertypeLinesShader);
+        private static final BufferBuilderRenderSink.RenderType LINES_LOOP = new BufferBuilderRenderSink.RenderType(VertexFormat.Mode.DEBUG_LINE_STRIP, DefaultVertexFormat.POSITION_COLOR_NORMAL, GameRenderer::getRendertypeLinesShader);
 
         private DefaultTypeFactory() {}
 
